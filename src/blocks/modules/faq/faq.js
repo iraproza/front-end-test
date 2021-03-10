db2689d996dc2ced207b46648b6c18bbebf2a150
Iraproza <kolomiira@gmail.com>
2021-03-10 21:15:01 +0200

    accordion js

--- src/blocks/modules/faq/faq.js
+++ src/blocks/modules/faq/faq.js
@@ -1,19 +1,15 @@
 let accordion = document.getElementsByClassName("accordion");
-console.log(accordion)
 
 for(let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
     let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-        panel.style.display = "none";
-    } else {
-        panel.style.display = "block";
-    }
-
-    if (panel.style.maxHeight){
+    this.classList.toggle('open');
+      panel.classList.toggle('open');
+      if (panel.style.maxHeight){
         panel.style.maxHeight = null;
       } else {
         panel.style.maxHeight = panel.scrollHeight + "px";
       }
-  });
+    });
+    
 }
